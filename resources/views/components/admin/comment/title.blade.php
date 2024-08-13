a803028cc87921a11daf123fd902500ae54897ca
Mohammadreza Norouzi <rda0044@gmail.com>
2024-08-13 14:35:21 +0330

    complete admin panel

--- resources/views/components/admin/comment/title.blade.php
+++ resources/views/components/admin/comment/title.blade.php
@@ -1,6 +1,3 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom ">
     <h1 class="h5 "><i class="fas fa-newspaper "></i> Comments</h1>
-    <div class="btn-toolbar mb-2 mb-md-0 ">
-        <a role="button " href="# " class="btn btn-sm btn-success disabled ">create</a>
-    </div>
 </div>
