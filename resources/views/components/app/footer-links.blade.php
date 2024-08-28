bb42990c72426229b0c6e0b8fa29463883971408
Mohammadreza Norouzi <rda0044@gmail.com>
2024-08-29 02:24:59 +0330

    create footer for application view

---
+++ resources/views/components/app/footer-links.blade.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-lg-6 col-md-6 single-footer-widget">
+        <h4>اخبار پربازدید</h4>
+        <ul>
+            <li><a href="#">عنوان خبر</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-6 col-md-6 single-footer-widget">
+        <h4>مرتبط با ما</h4>
+        <ul>
+            <li><a href="#">درباره ما</a></li>
+            <li><a href="#">تماس با ما</a></li>
+            <li><a href="#">سوالات متداول</a></li>
+        </ul>
+    </div>
+</div>
