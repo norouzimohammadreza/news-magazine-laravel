a7584069c59fbaf293679346960f7ec5f6532935
newsa720 <rda0044@gmail.com>
2024-10-29 17:12:14 +0330

    add user factory and seeder

--- app/Http/Requests/Api/Auth/RegisterRequest.php
+++ app/Http/Requests/Api/Auth/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends ApiFormRequest
             'name' => 'required|min:5|max:50|unique:users',
             'email' => 'required|email|max:100|unique:users',
             'password' => 'required|min:8|max:48|confirmed',
-            'password_confirmation' => 'required|min:8|max:48|same:password|regex:^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$',
+            'password_confirmation' => 'required|min:8|max:48|same:password|regex:^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$^',
         ];
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,11 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
+
 class UserFactory extends Factory
 {
     /**
@@ -26,9 +23,9 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= Hash::make('1234@Abcd'),
+            'is_active' => rand(0, 1),
+
         ];
     }
 
