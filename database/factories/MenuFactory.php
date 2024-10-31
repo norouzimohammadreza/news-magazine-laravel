4afd766562a423edf769ef7ac9a784a7e9c64345
newsa720 <rda0044@gmail.com>
2024-10-31 11:23:36 +0330

    create category and menu factories and seeders

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->word(),
+            'url' => fake()->url,
+            'parent_id' => function () {
+                return Menu::factory()->create()->id;
+            }
+        ];
+    }
+}
