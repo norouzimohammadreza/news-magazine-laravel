8ed8ca0b782ed7d610382bffb17c28556ee78827
norouzimohammadreza <rda0044@gmail.com>
2025-03-11 15:54:59 +0330

    call web scraper service at schedule on every two minutes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Services\WebScraper;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $crawlerService = new WebScraper();
+    $crawlerService->fetchNews('www.google.com');
+})->everyTwoMinutes();
