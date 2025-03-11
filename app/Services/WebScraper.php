992b78d0e7d9e67f305938cbd33af9f5178b8d87
norouzimohammadreza <rda0044@gmail.com>
2025-03-11 15:24:32 +0330

    Create crawler for fetch news by http client

--- app/Services/WebScraper.php
+++ app/Services/WebScraper.php
@@ -2,7 +2,20 @@
 
 namespace App\Services;
 
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\DomCrawler\Crawler;
+
 class WebScraper
 {
+    protected HttpBrowser $client;
 
+    public function __construct()
+    {
+        $this->client = new HttpBrowser(HttpClient::create());
+    }
+    public function fetchNews(string $url)
+    {
+        $crawler = $this->client->request('GET', $url);
+    }
 }
